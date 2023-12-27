2566ad854da077155de8add543b976818ceb5025
chohyunwoo <wizardchw@naver.com>
2023-12-27 22:28:12 +0900

    ..

--- first/src/main/java/org/example/first/FirstServiceController.java
+++ first/src/main/java/org/example/first/FirstServiceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
--- second/src/main/java/org/example/second/SecondServiceController.java
+++ second/src/main/java/org/example/second/SecondServiceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
