030585f5a1efb41ec2873ac4e5848dc4940fd403
chohyunwoo <wizardchw@naver.com>
2023-12-27 22:05:06 +0900

    라우팅 역할을 하는 zuul service 설정 및 테스트 해보기 (spring boot 3 버전 대에서는 지원하지 않음)

---
+++ first/src/main/java/org/example/first/FirstServiceController.java
@@ -0,0 +1,16 @@
+package org.example.first;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class FirstServiceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to First Service";
+    }
+}
---
+++ second/src/test/java/org/example/second/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
