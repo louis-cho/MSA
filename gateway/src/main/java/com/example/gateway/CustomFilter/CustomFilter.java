23b25f9a724a1b36a3931cb5a4d2daef795df3a9
chohyunwoo <wizardchw@naver.com>
2024-01-01 11:22:58 +0900

    custom filter 완성

---
+++ gateway/src/main/java/com/example/gateway/CustomFilter/CustomFilter.java
@@ -0,0 +1,35 @@
+package com.example.gateway.CustomFilter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<AbstractGatewayFilterFactory.NameConfig> {
+
+    public CustomFilter() {
+        super(NameConfig.class);
+    }
+
+    @Override
+    public GatewayFilter apply(NameConfig config) {
+        return (exchange, chain) -> {
+          ServerHttpRequest request = exchange.getRequest();
+          ServerHttpResponse response = exchange.getResponse();
+
+          log.info("Custom PRE filter : request id -> {}", request.getId());
+
+          // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(()-> {
+                log.info("Custom PRE filter : response id -> {}", response.getStatusCode());
+            }));
+        };
+    }
+
+
+}
